7324a578535dca55508e48e42416abcd59a959c7
Virakyuth Srun <156297649+virakyuthSRUN@users.noreply.github.com>
2024-04-07 17:04:27 +0700

    Update main.java

--- main.java
+++ main.java
@@ -1,68 +1,68 @@
 import java.util.Scanner;
 
-public class StudentMain {
+public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String reply="";
-		StudentLogic obj = new StudentLogic(); 
-		do
-		{
-			System.out.println("WELCOME TO STUDENT MANAGEMENT SYSTEM");
-			System.out.println("1. ADD STUDENT ");
-			System.out.println("2. DELETE STUDENT ");
-			System.out.println("3. SERACH STUDENT ");
-			System.out.println("4. UPDATE STUDENT DETAILS ");
-			System.out.println("5. DISPLAY ALL STUDENT ");
-			
-			System.out.println("ENTER YOUR CHOICE ");
-			int option = sc.nextInt();
-			
-			switch(option)
-			{
-			   case 1: obj.readArrayListFromFile();
-				       obj.addStudent();
-			           obj.writeArrayListToFile();
-				       break;
-			 
-			   case 2: obj.readArrayListFromFile();
-		               obj.deleteStudent(); 
-		               obj.writeArrayListToFile();
-	                   break;
-					
-	            //add and update item
-			   case 1: obj.readArrayListFromFile();
-				       obj.addItem();
-			           obj.writeArrayListToFile();
-				       break;
-			 
-			   case 2: obj.readArrayListFromFile();
-		               obj.updateItemByID(); 
-		               obj.writeArrayListToFile();
-	                   break;
-
-
-					
-			   case 3: obj.readArrayListFromFile();
-                       obj.searchStudentById(); 
-                       break;
-			   
-			   case 4: obj.readArrayListFromFile();
-		               obj.updateStudentDetailsByID();
-		               obj.writeArrayListToFile();
-	                   break;
-			   
-			   case 5: obj.readArrayListFromFile();
-				       obj.displayAllStudent(); 
-			           break;
-			   
-			   default:
-				   System.out.println("PLEASE ENTER VALID INPUT ");
-			}
-			
-			
-			System.out.println("DO YOU WANT TO CONTINUE Y/N ");
-			reply = sc.next();
-		}while(reply.equalsIgnoreCase("y"));
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String reply="";
+        InventoryLogic obj = new InventoryLogic(); 
+        do {
+            System.out.println("WELCOME TO INVENTORY MANAGEMENT SYSTEM");
+            System.out.println("1. ADD PRODUCT ");
+            System.out.println("2. DELETE PRODUCT ");
+            System.out.println("3. SEARCH PRODUCT ");
+            System.out.println("4. UPDATE PRODUCT DETAILS ");
+            System.out.println("5. DISPLAY ALL PRODUCTS ");
+            System.out.println("6. GENERATE REPORT");
+            System.out.println("7. SALE ORDER MANAGEMENT");
+            System.out.println("8. PURCHASE ORDER (PO)");
+            System.out.println("9. EXIT");
+            
+            System.out.println("ENTER YOUR CHOICE ");
+            int option = sc.nextInt();
+            
+            switch(option) {
+                case 1:
+                    obj.readArrayListFromFile();
+                    obj.addProduct();
+                    obj.writeArrayListToFile();
+                    break;
+                case 2:
+                    obj.readArrayListFromFile();
+                    obj.deleteProduct();
+                    obj.writeArrayListToFile();
+                    break;
+                case 3:
+                    obj.readArrayListFromFile();
+                    obj.searchProductById();
+                    break;
+                case 4:
+                    obj.readArrayListFromFile();
+                    obj.updateProductDetailsById();
+                    obj.writeArrayListToFile();
+                    break;
+                case 5:
+                    obj.readArrayListFromFile();
+                    obj.displayAllProducts();
+                    break;
+                case 6:
+                    obj.generateReport();
+                    break;
+                case 7:
+                    obj.saleOrderManagement();
+                    break;
+                case 8:
+                    obj.purchaseOrder();
+                    break;
+                case 9:
+                    System.out.println("Thank you for using the Inventory Management System!");
+                    System.exit(0);
+                default:
+                    System.out.println("PLEASE ENTER VALID INPUT ");
+            }
+            
+            System.out.println("DO YOU WANT TO CONTINUE Y/N ");
+            reply = sc.next();
+        } while(reply.equalsIgnoreCase("y"));
+    }
 }
