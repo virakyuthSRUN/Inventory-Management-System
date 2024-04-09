2ca69ba00cc60d02286c56763cd3ed5331fa5d09
Virakyuth Srun <156297649+virakyuthSRUN@users.noreply.github.com>
2024-04-09 08:18:49 +0700

    Update InventoryMain.java

--- InventoryMain.java
+++ InventoryMain.java
@@ -3,52 +3,66 @@ import java.util.Scanner;
 public class InventoryMain {
 
     public static void main(String[] args) {
+    	
+    	
         Scanner sc = new Scanner(System.in);
         String reply="";
         InventoryLogic obj = new InventoryLogic(); 
-
+        boolean isLoggedIn = false;
+        
         do {
-            System.out.println("WELCOME TO STUDENT MANAGEMENT SYSTEM");
-            System.out.println("1. REGISTER");
-            System.out.println("2. LOGIN");
-            System.out.println("3. EXIT");
-
-            System.out.println("ENTER YOUR CHOICE ");
-            int option = sc.nextInt();
-
-            switch (option) {
-                case 1:
-                    obj.register();
-                    obj.login();
-                    break;
-
-                case 2:
-                    obj.login();   
-
-                    break;
-
-                case 3:
-                    System.out.println("Exiting Inventory Management System.");
-                    return;
-
-            default:
-                System.out.println("PLEASE ENTER VALID INPUT ");
-        }
-        } while (reply.equalsIgnoreCase("y"));
-           
-        do {
-            System.out.println("WELCOME TO INVENTORY MANAGEMENT SYSTEM");
-            System.out.println("1. ADD PRODUCT ");
-            System.out.println("2. DELETE PRODUCT ");
-            System.out.println("3. SEARCH PRODUCT ");
-            System.out.println("4. UPDATE PRODUCT DETAILS ");
-            System.out.println("5. DISPLAY ALL PRODUCTS ");
-            System.out.println("6. GENERATE REPORT");
-            System.out.println("7. SALE ORDER MANAGEMENT");
-            System.out.println("8. PURCHASE ORDER (PO)");
-            System.out.println("9. EXIT");
+        	System.out.println("");
+            System.out.println("🤖 WELCOME TO INVENTORY MANAGEMENT SYSTEM");
+            System.out.println("1. Admin Login ");
+            System.out.println("2. EXIT");
+            System.out.println("");
             
-            System.out.println("ENTER YOUR CHOICE ");
+            System.out.print("ENTER YOUR CHOICE\n> ");
+            int option = sc.nextInt();
+            
+            switch(option) {
+                case 1:
+                    isLoggedIn = obj.adminLogin();
+                    if(isLoggedIn) {
+                    	System.out.println("");
+                        System.out.println("Login Successful!");
+                        inventoryMenu(obj);
+                    } else {
+                    	System.out.println("");
+                        System.out.println("Login Failed. Please try again.");
+                    }
+                    break;
+                case 2:
+                	System.out.println("");
+                    System.out.println("Thank you for using the Inventory Management System!");
+                    System.exit(0);
+                default:
+                	
+                    System.out.println("PLEASE ENTER VALID INPUT ");
+            }
+            
+            System.out.println("");
+            System.out.print("🤖 DO YOU WANT TO CONTINUE? (Y/N)\n> ");
+            reply = sc.next();
+        } while(reply.equalsIgnoreCase("y"));
+    }
+    
+    public static void inventoryMenu(InventoryLogic obj) {
+        Scanner sc = new Scanner(System.in);
+        String reply="";
+        do {	
+        	System.out.println("");
+            System.out.println("1. Add Product ");
+            System.out.println("2. Update Product ");
+            System.out.println("3. Remove Product ");
+            System.out.println("4. Search Product ");
+            System.out.println("5. Display Product ");
+            System.out.println("6. Sale Order Management ");
+            System.out.println("7. Purchase Order (PO) ");
+            System.out.println("8. Logout");
+            System.out.println("");
+            
+            System.out.print("🤖 ENTER YOUR CHOICE\n> ");
             int option = sc.nextInt();
             
             switch(option) {
@@ -59,39 +73,37 @@ public class InventoryMain {
                     break;
                 case 2:
                     obj.readArrayListFromFile();
-                    obj.deleteProduct();
+                    obj.updateProduct();
                     obj.writeArrayListToFile();
                     break;
                 case 3:
                     obj.readArrayListFromFile();
-                    obj.searchProductById();
+                    obj.removeProduct();
+                    obj.writeArrayListToFile();
                     break;
                 case 4:
                     obj.readArrayListFromFile();
-                    obj.updateProductDetailsById();
-                    obj.writeArrayListToFile();
+                    obj.searchProduct();
                     break;
                 case 5:
                     obj.readArrayListFromFile();
                     obj.displayAllProducts();
                     break;
                 case 6:
-                    obj.generateReport();
-                    break;
-                case 7:
                     obj.saleOrderManagement();
                     break;
-                case 8:
+                case 7:
                     obj.purchaseOrder();
                     break;
-                case 9:
-                    System.out.println("Thank you for using the Inventory Management System!");
-                    System.exit(0);
+                case 8:
+                    System.out.println("Logged out successfully.");
+                    return;
                 default:
                     System.out.println("PLEASE ENTER VALID INPUT ");
             }
             
-            System.out.println("DO YOU WANT TO CONTINUE Y/N ");
+            System.out.println("");
+            System.out.print("🤖 DO YOU WANT TO CONTINUE (Y/N)\n> ");
             reply = sc.next();
         } while(reply.equalsIgnoreCase("y"));
     }
